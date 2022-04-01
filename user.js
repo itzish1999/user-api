0cd0d157c5e70a32532203f2471be44506df5a1b
itzish1999 <ismaeel.butt19@gmail.com>
2022-04-01 19:34:52 -0400

    Very basic auth for the backend

--- user.js
+++ user.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const app = express()
+const basicAuth = require('express-basic-auth')
 
-app.get('/users', (req, res) => {
-    res.end('User page');
+
+app.get('/', (req, res) => {
+    console.log('Home Page')
+    res.send('Home Page')
+
+});
+
+app.use(basicAuth({
+    users: {'admin': 'skltpsyxm786786'}
+
+}));
+
+
+app.get('/user', (req, res) => {
+    console.log('User Page')
+    res.send('User page')
 });
 
 app.listen(8080, () => {
