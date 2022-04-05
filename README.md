99bd2999bfe6690287cbbc38884e1e402b756d95
itzish1999 <ismaeel.butt19@gmail.com>
2022-04-05 15:24:24 -0400

    Basic linking of database

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # user-api
+
+This is the api that I will be using for Users.
+The longterm goal is to have:
+1 api for authentication
+1 api for data management
+1 api for email notifications
+1 api for frontend work
+
+I will put the queries.js in a separate file with restrictive permissions but for the basic intorduction of this code I will keep it within the users-api folder. Plus I have much to learn about that!
--- user.js
+++ user.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const app = express()
-const basicAuth = require('express-basic-auth')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const db = require('./queries')
 
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
 app.get('/', (req, res) => {
     console.log('Home Page')
@@ -9,16 +16,16 @@ app.get('/', (req, res) => {
 
 });
 
-app.use(basicAuth({
-    users: {'admin': 'skltpsyxm786786'}
-
-}));
-
-
-app.get('/user', (req, res) => {
+app.get('/users', (req, res) => {
     console.log('User Page')
-    res.send('User page')
+    res.json({ info: 'Node.js, Express, and Postgres API' })
 });
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(8080, () => {
     console.log('Node server running on port 8080')
